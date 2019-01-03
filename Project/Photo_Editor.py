a21ec4997dff977f07c687c77301e797cda84f29
fyodorussia <fvdelows@mail.ru>
2019-01-03 16:17:03 +0300

    добавил коментарии исправил размер поля поменял поворот картинки исравил востановление положения slider-а

--- Project/Photo_Editor.py
+++ Project/Photo_Editor.py
@@ -10,6 +10,7 @@ from PIL import Image as I
 class Error(QWidget):
     def __init__(self, message):
         super().__init__()
+        # создаю окно ошибки и вывожу текст ошибки message
         self.setGeometry(800, 470, 230, 80)
         self.setWindowTitle('ERROR')
         self.txt = QLabel(self)
@@ -21,19 +22,20 @@ class Error(QWidget):
 class Photo_Editor(QMainWindow):
     def __init__(self):
         super().__init__()
+        # подключаю дизайн
         uic.loadUi('дизайн.ui', self)
-        self.tools = Editor_tools('init.jpg', 1310, 920, self)
-        self.base()
-
-    def base(self):
+        # подключаю класс функций и заливаю основные параметры изображения
+        self.tools = Editor_tools('init.jpg', 900, 920, self)
+        # вывожу изначальную катринку
         self.lbl.setPixmap(QPixmap(self.tools.name))
+        # подключаю кнопки к соответствующим функциям
         self.btn_new.clicked.connect(self.tools.new_picture)
         self.btn_copy.clicked.connect(self.tools.copy_picture)
         self.rec_r.clicked.connect(self.tools.recoloring_picture)
         self.rec_g.clicked.connect(self.tools.recoloring_picture)
         self.rec_b.clicked.connect(self.tools.recoloring_picture)
-        self.btn_roted_l.clicked.connect(self.tools.roted_r)
-        self.btn_roted_r.clicked.connect(self.tools.roted_l)
+        self.btn_roted_l.clicked.connect(self.tools.roted_l)
+        self.btn_roted_r.clicked.connect(self.tools.roted_r)
         self.btn_sepia.clicked.connect(self.tools.sepia)
         self.btn_black.clicked.connect(self.tools.black)
         self.btn_negativ.clicked.connect(self.tools.negative)
@@ -42,152 +44,165 @@ class Photo_Editor(QMainWindow):
 
 class Editor_tools:
     def __init__(self, name, x, y, base):
-        self.name = name
-        self.x = x
+        # прогружаю основные данные об изображении
+        self.name = name  # имя
+        self.x = x  # размеры
         self.y = y
-        self.base = base
-        self.copy_ind = 1
-        self.old_proc_r = 100
+        self.base = base  # создаю ссылку на основной класс
+        self.copy_ind = 1  # сколько копий у этого изображения
+        self.old_proc_r = 100  # процентные составляющие цветов
         self.old_proc_g = 100
         self.old_proc_b = 100
-        self.im = I.open(self.name)
+        self.im = I.open(self.name)  # загружаю изображение
 
-    def copy_picture(self):
+    def copy_picture(self):  # функция копирования
+        # спрашиваю имя копии
         i, okBtnPressed = QInputDialog.getText(self.base,
                                                "Введите название картинки",
                                                "Копия")
         if okBtnPressed:
             if not (os.path.isfile(i)):
-                if i == '':
+                if i == '':  # если не указано новое имя действую по умолчанию (+(copy_ind))
                     str = '({}).'.format(self.copy_ind)
                     if os.path.isfile(
                             str.join(self.name.split('.'))):
                         self.copy_ind += 1
                         str = '({}).'.format(self.copy_ind)
                     i = str.join(self.name.split('.'))
-                self.im.save(i)
-                self.exit_picture(i)
-            else:
+                self.im.save(i)  # создаю копию
+                self.exit_picture(i)  # вывожу картинку
+            else:  # вывожу при ошибке предупреждение
                 self.error1 = Error('Указанный файл уже существует')
                 self.error1.show()
 
-    def new_picture(self):
+    def new_picture(self):  # функция новой картинки
+        # спрашиваю имя новой картинки
         i, okBtnPressed = QInputDialog.getText(self.base,
                                                "Введите название картинки",
                                                "Картинка")
         if okBtnPressed:
+            # привожу значения по умолчанию
             self.copy_ind = 1
-            self.exit_picture(i)
+            self.old_proc_r = 100
+            self.old_proc_g = 100
+            self.old_proc_b = 100
+            self.base.proc_r_sp.setValue(100)
+            self.base.proc_g_sp.setValue(100)
+            self.base.proc_b_sp.setValue(100)
+            self.exit_picture(i)  # вывожу картинкку
 
-    def exit_picture(self, i):
-        if not (os.path.isfile(i)):
+    def exit_picture(self, i):  # функция вывода
+        if not (os.path.isfile(i)):  # проаверка на существование файла
             self.error1 = Error('Указанный файл не существует')
-            self.error1.show()
-        else:
+            self.error1.show()  # вывод предупреждения
+        else:  # вывод картинки
             self.im = I.open(i)
             x, y = self.im.size
-            if 1310 < x or 920 < y:
+            if 900 < x or 920 < y:  # проверка на корректность размеров картинки
                 self.error2 = Error(
-                    'Неверный размер картинки\nмаксимальный размер 1310x920')
-                self.error2.show()
+                    'Неверный размер картинки\nмаксимальный размер 900x920')
+                self.error2.show()  # вывод предупреждения
             else:
-                self.base.lbl.resize(x, y)
-                self.base.lbl.move(570 + 655 - x // 2, 490 - y // 2)
-                self.name = i
-                self.x = x
+                self.base.lbl.resize(x, y)  # изменяю поле для изображения
+                self.base.lbl.move(570 + 450 - x // 2,
+                                   490 - y // 2)  # преремещаю поле, чтобы новая картинка была посередине
+                # меняю параметры изображения
+                self.name = i  # имя
+                self.x = x  # размеры
                 self.y = y
-                self.old_proc_r = 100
-                self.old_proc_g = 100
-                self.old_proc_b = 100
-                self.base.proc_r_sp.setValue(100)
-                self.base.proc_g_sp.setValue(100)
-                self.base.proc_b_sp.setValue(100)
-                self.base.lbl.setPixmap(QPixmap(self.name))
+                self.base.lbl.setPixmap(QPixmap(self.name))  # вывожу картинку в поле
 
-    def roted_l(self):
-        if self.name != 'init.jpg':
-            if 1310 < self.y or 920 < self.x:
+    def roted_l(self):  # функция поворота картинки против часовой
+        if self.name != 'init.jpg':  # проверка на начальную картинку
+            if 900 < self.y or 920 < self.x:  # проверка на корректность размеров
                 self.error2 = Error(
-                    'Невозможно перевернуть картинку\nмаксимальный размер 1310x920')
-                self.error2.show()
+                    'Невозможно перевернуть картинку\nмаксимальный размер 900x920')
+                self.error2.show()  # вывод предупреждения
             else:
-                self.im.transpose(I.ROTATE_270).save(self.name)
-                self.exit_picture(self.name)
+                self.im.transpose(I.ROTATE_90).save(self.name)  # поворот картинки
+                self.exit_picture(self.name)  # вывод картинки
 
-    def roted_r(self):
-        if self.name != 'init.jpg':
-            if 1310 < self.y or 920 < self.x:
+    def roted_r(self):  # функция поворота картинки по часовой
+        if self.name != 'init.jpg':  # проверка на начальную картинку
+            if 900 < self.y or 920 < self.x:  # проверка на корректность размеров
                 self.error2 = Error(
-                    'Невозможно перевернуть картинку\nмаксимальный размер 1310x920')
-                self.error2.show()
+                    'Невозможно перевернуть картинку\nмаксимальный размер 900x920')
+                self.error2.show()  # вывод предупреждения
             else:
-                self.im.transpose(I.ROTATE_90).save(self.name)
-                self.exit_picture(self.name)
+                self.im.transpose(I.ROTATE_270).save(self.name)  # поворот картинки
+                self.exit_picture(self.name)  # вывод картинки
 
-    def sepia(self):
-        if self.name != 'init.jpg':
-            pixels = self.im.load()
+    def sepia(self):  # функция фильтра сепия
+        if self.name != 'init.jpg':  # проверка на начальную картинку
+            pixels = self.im.load()#загружаю пиксели
+            #изменяю цвет пикселей
             for i in range(self.x):
                 for j in range(self.y):
-                    r, g, b = pixels[i, j]
-                    o = (r + g + b) // 3
-                    pixels[i, j] = int(o + 48), int(o + 32), int(o)
-            self.im.save(self.name)
-            self.exit_picture(self.name)
+                    col = pixels[i, j]#загружаю цвет
+                    o = (col[0] + col[1] + col[2]) // 3#считаю среднюю цветовую составляющую
+                    pixels[i, j] = int(o + 48), int(o + 32), int(o)#меняю цвет пикселя
+            self.im.save(self.name)#сохраняю картинку
+            self.exit_picture(self.name)#вывожу картинку
 
     def negative(self):
-        if self.name != 'init.jpg':
-            pixels = self.im.load()
+        if self.name != 'init.jpg':  # проверка на начальную картинку
+            pixels = self.im.load()#загружаю пиксели
+            #изменяю цвет пикселей
             for i in range(self.x):
                 for j in range(self.y):
-                    r, g, b = pixels[i, j]
-                    pixels[i, j] = 255 - r, 255 - g, 255 - b
-            self.im.save(self.name)
-            self.exit_picture(self.name)
+                    col = pixels[i, j]#загружаю цвет
+                    pixels[i, j] = 255 - col[0], 255 - col[1], 255 - col[2]#меняю цвет пикселя
+            self.im.save(self.name)#сохраняю картинку
+            self.exit_picture(self.name)#вывожу картинку
 
     def black(self):
-        if self.name != 'init.jpg':
-            pixels = self.im.load()
+        if self.name != 'init.jpg':  # проверка на начальную картинку
+            pixels = self.im.load()  # загружаю пиксели
             for i in range(self.x):
                 for j in range(self.y):
-                    r, g, b = pixels[i, j]
-                    c = (r + g + b) // 3
-                    pixels[i, j] = c, c, c
-            self.im.save(self.name)
-            self.exit_picture(self.name)
+                    col = pixels[i, j]  # загружаю цвет
+                    c = (col[0] + col[1] + col[2]) // 3  # создаю основной цвет
+                    pixels[i, j] = c, c, c  # изменяю цвет пикселя
+            self.im.save(self.name)  # сохраняю изображения
+            self.exit_picture(self.name)  # вывожу картинку
 
-    def frame(self):
-        if self.name != 'init.jpg':
-            color = QColorDialog.getColor()
-            fr = self.base.wheight.value()
+    def frame(self):  # функция рамки
+        if self.name != 'init.jpg':  # проверка на начальную картинку
+            color = QColorDialog.getColor()  # получаю цвет рамки
+            fr = self.base.wheight.value()  # получаю толщину рамки
             im_n = I.new("RGB", (self.x + fr * 2, self.y + fr * 2),
-                         color.name())
-            pixels = self.im.load()
-            pixels1 = im_n.load()
+                         color.name())  # создаю монотонное изображение ("рамку") с учетом толщины рамки и её цвета
+            pixels = self.im.load()  # загружаю пиксели
+            pixels1 = im_n.load()  # загружаю пиксели "рамки"
+            # загружаю в "рамку" начальное изображение
             for i in range(self.x):
                 for j in range(self.y):
                     pixels1[i + fr, j + fr] = pixels[i, j]
-            self.im = im_n
-            self.x += fr * 2
+            self.im = im_n  # меняю изображение
+            self.x += fr * 2  # меняю размеры
             self.y += fr * 2
-            im_n.save(self.name)
-            self.exit_picture(self.name)
+            im_n.save(self.name)  # сохраняю изображение
+            self.exit_picture(self.name)  # вывожу изображение
 
-    def recoloring_picture(self):
-        if self.name != 'init.jpg':
-            pixels = self.im.load()
+    def recoloring_picture(self):  # функция изменения цветовой составляющей
+        if self.name != 'init.jpg':  # проверка на начальную картинку
+            pixels = self.im.load()  # загружаю пиксели
+            # изменяю цветовую составляющую
             for i in range(self.x):
                 for j in range(self.y):
-                    r, g, b = pixels[i, j]
+                    col = pixels[i, j]  # загружаю цвет
+                    # считаю изменение цвета для каждой составляющей
                     d_r = self.base.proc_r_sp.value() / self.old_proc_r
                     d_g = self.base.proc_g_sp.value() / self.old_proc_g
                     d_b = self.base.proc_b_sp.value() / self.old_proc_b
-                    pixels[i, j] = int(r * d_r), int(g * d_g), int(b * d_b)
+                    pixels[i, j] = int(col[0] * d_r), int(col[1] * d_g), int(
+                        col[2] * d_b)  # изменяю цвет пикселя
+            #меняю процент цвета
             self.old_proc_r = self.base.proc_r_sp.value()
             self.old_proc_g = self.base.proc_g_sp.value()
             self.old_proc_b = self.base.proc_b_sp.value()
-            self.im.save(self.name)
-            self.exit_picture(self.name)
+            self.im.save(self.name)#сохраняю изображение
+            self.exit_picture(self.name)#вывожу изображение
 
 
 if __name__ == '__main__':
